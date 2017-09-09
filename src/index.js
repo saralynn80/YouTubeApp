8897fd7b4a73b2209cd47c192c087ef9e211518f
Sara Hudanich <sara.hudanich@gmail.com>
2017-09-09 13:23:32 -0400

    Callbacks

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
-import React from 'react'; // Reacts with components
-import ReactDOM from 'react-dom'; // Reacts with the dom
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import SearchBar from './components/search_bar'; // Import the search bar component
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCxaM8_iwrErYJtOdemm4TkDX8uhyhkY6w';
 
-YTSearch({key: API_KEY, term: 'cats'}, function(data) {
-	console.log(data);
-});
+class App extends Component {
+	constructor(props) {
+		super(props);
 
-// Create a new component. This component should produce some html
-// http://babeljs.io/repl/ will show what the JSX will render
+		//this.state = { videos: [] };
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
 
-//const App = function() {
-	//return <div>Hi!</div>;
-//}
+		YTSearch({key: API_KEY, term: 'cats'}, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
 
-const App = () => {
-	return(
-	<div>
-		<SearchBar />
-	</div>
-	);
+	render() {
+		return(
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
-// ReactDOM.render(<component instance/>, target on the page)
 ReactDOM.render(<App />, document.querySelector('.container'));
